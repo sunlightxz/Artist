b677e18bec9f2019cc96afe47265928fbeb70468
Amine As <amineasfar101@gmail.com>
2024-08-30 14:55:31 +0100

    some chasdngs

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -1,71 +1,82 @@
-import React from 'react'
-import og from '/OG.webp'
-import main from '/Main.webp'
-import { useEffect ,useRef} from 'react';
+import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import og from '/OG.webp';
+import main from '/Main.webp';
+import lck from '/lck.webp';
 
+gsap.registerPlugin(ScrollTrigger);
 const More = () => {
-
-  const ogpicture = useRef();
-  const mainpicture = useRef();
-
+   
   useEffect(() => {
-    gsap.set(".trigger", {
-      clipPath: function () {
-        return "inset(0px 0px 0px 0px)";
+    gsap.fromTo('.cont-paint', 
+      { clipPath: 'inset(100% 0 0 0)' }, // Initial state: completely clipped
+      {
+        clipPath: 'inset(0 0 0 0)', // Final state: fully visible
+        duration: 1,
+        ease: 'power2.out',
+        stagger: 0.5,
+        scrollTrigger: {
+          trigger: '.cont-paint',
+          start: 'top 80%',
+          end: 'bottom 20%',
+          scrub: true,
+          markers: true,
+        },
       }
-    });
-
-    const animation = gsap.to(".original-painting", {
-      clipPath: function () {
-        return "inset(0px 0px 100% 0px)";
-      },
-      ease: "none",
-    });
-
-    ScrollTrigger.create({
-      trigger: '.cont-paint',
-      start: 'top top',
-      end: 'bottom bottom',
-      animation: animation,
-      scrub: 1,
-    });
+    );
   }, []);
 
   return (
     <section className="web-content relative w-full bg-white z-50">
       <div className="">
         <div className="sticky top-0 h-screen cont-paint">
-          <div className="absolute left-0 right-0 top-0 z-20  w-full font-nippo  text-lemon">
-            <div className="section-padding grid-gap  sticky top-0 flex h-screen grid-cols-12 flex-col justify-center  gap-y-fluid-sm md:grid md:place-items-center md:items-center">
+          <div className="absolute left-0 right-0 top-0 z-20 w-full font-nippo text-lemon " style={{ height: '300vh', top: '0vh' }}>
+            <div className="section-padding grid-gap  sticky top-0 flex h-screen grid-cols-12 flex-col justify-center gap-y-fluid-sm md:grid md:place-items-center md:items-center">
               <span className="col-span-4 flex text-heading-sm tracking-heading">
                 <span className='text-3xl text-red-500'>TNOE</span>
               </span>
               <div className=" group relative col-span-4 flex aspect-square w-full items-center justify-center overflow-clip rounded-xl">
                 <div className="relative z-50 h-full w-full mix-blend-difference"></div>
-                <img src={main} className='original-painting pointer-events-none absolute h-full w-full object-cover trigger' alt="" />
+                  <img src={main} className='original-painting pointer-events-none absolute h-full w-full object-cover smallImg' alt="" />
+                </div>
+            </div>
+          </div>
+          <div className="relative h-fit w-full">
+            <img src={main} className='absolute h-screen w-full object-cover' width={1200} height={1421} alt="" />
+          </div>
+        </div>
+      
+        <div className="sticky top-0 h-screen cont-paint">
+          <div className="absolute left-0 right-0 top-0 z-20 w-full font-nippo text-lemon " style={{ height: '300vh', top: '-100vh' }}>
+            <div className="section-padding grid-gap  sticky top-0 flex h-screen grid-cols-12 flex-col justify-center gap-y-fluid-sm md:grid md:place-items-center md:items-center">
+              <span className="col-span-4 flex text-heading-sm tracking-heading">
+                <span className='text-3xl text-red-500'>mweofsd</span>
+              </span>
+              <div className=" group relative col-span-4 flex aspect-square w-full items-center justify-center overflow-clip rounded-xl">
+                <div className="relative z-50 h-full w-full mix-blend-difference"></div>
+                <img src={og} className='original-painting pointer-events-none absolute h-full w-full object-cover smallImg3' alt="" />
               </div>
             </div>
           </div>
-          <div className=" relative h-fit w-full">
-            <img src={main} className='absolute h-screen w-full object-cover' width={1200} height={1421} alt="" ref={ogpicture}/>
+          <div className="relative h-fit w-full">
+            <img src={og} className='absolute h-screen w-full object-cover' width={1200} height={1421} alt="" />
           </div>
         </div>
         <div className="sticky top-0 h-screen cont-paint">
-          <div className="absolute left-0 right-0 top-0 z-20  w-full font-nippo  text-lemon">
-            <div className="section-padding grid-gap  sticky top-0 flex h-screen grid-cols-12 flex-col justify-center  gap-y-fluid-sm md:grid md:place-items-center md:items-center">
+          <div className="absolute left-0 right-0 top-0 z-20  w-full font-nippo text-lemon " style={{ height: '300vh', top: '-200vh' }}>
+            <div className="section-padding grid-gap  sticky top-0 flex h-screen grid-cols-12 flex-col justify-center gap-y-fluid-sm md:grid md:place-items-center md:items-center">
               <span className="col-span-4 flex text-heading-sm tracking-heading">
-                <span className='text-3xl text-red-500'>TNOE</span>
+                <span className='text-3xl text-red-500'>meowsss</span>
               </span>
               <div className=" group relative col-span-4 flex aspect-square w-full items-center justify-center overflow-clip rounded-xl">
                 <div className="relative z-50 h-full w-full mix-blend-difference"></div>
-                <img src={og} className='original-painting pointer-events-none absolute h-full w-full object-cover' alt="" />
+                <img src={lck} className='original-painting pointer-events-none absolute h-full w-full object-cover smallImg3' alt="" />
               </div>
             </div>
           </div>
-          <div className=" relative h-fit w-full">
-            <img src={og} className='absolute h-screen w-full object-cover' width={1200} height={1421} alt="" ref={mainpicture} />
+          <div className="relative h-fit w-full">
+            <img src={lck} className='absolute h-screen w-full object-cover' width={1200} height={1421} alt="" />
           </div>
         </div>
       </div>
